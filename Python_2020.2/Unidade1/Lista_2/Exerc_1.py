cc6046c5ab9e201aad0f25f3a79c07533a3323a5
Gabriel Lira <106107461+Gabrielgln@users.noreply.github.com>
2022-09-09 07:13:09 -0300

    Add files via upload

---
+++ Python_2020.2/Unidade1/Lista_2/Exerc_1.py
@@ -0,0 +1,7 @@
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+letra = input('Digite uma letra: ')
+if letra == 'a' or letra == 'e' or letra =='i' or letra =='o' or letra =='u':
+  print('é uma vogal')
+else:
+  print('é uma consoante') 
